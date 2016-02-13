fcc722d78c61a52fe85a27313711dce759f2863f
Max Murdoch <max@maxmurdo.ch>
2016-02-13 14:18:16 +0000

    Update custom.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,15 +7,23 @@
    ["#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#81a2be" "#c5c8c6"])
  '(ansi-term-color-vector
    [unspecified "#1d1f21" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#81a2be" "#c5c8c6"] t)
+ '(column-number-mode t)
  '(custom-safe-themes
    (quote
-    ("3a3917dbcc6571ef3942c2bf4c4240f70b5c4bc0b28192be6d3f9acd83607a24" default)))
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "b9183de9666c3a16a7ffa7faaa8e9941b8d0ab50f9aaba1ca49f2f3aec7e3be9" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "232f715279fc131ed4facf6a517b84d23dca145fcc0e09c5e0f90eb534e1680f" "7c1e99f9d46c397b3fd08c7fdd44fe47c4778ab69cc22c344f404204eb471baa" "f2503f0a035c2122984e90eb184185769ee665de5864edc19b339856942d2d2d" "7e346cf2cb6a8324930c9f07ce050e9b7dfae5a315cd8ed3af6bcc94343f8402" "a922c743710bb5d7c14995345549141f01211ff5089057dc718a5a33104c3fd1" "3a3917dbcc6571ef3942c2bf4c4240f70b5c4bc0b28192be6d3f9acd83607a24" default)))
+ '(global-hl-line-mode t)
+ '(line-number-mode t)
  '(package-selected-packages
    (quote
-    (fold-dwim emacsql-sqlite web-mode markdown-mode+ yaml-mode rainbow-delimiters auto-indent-mode indent-guide helm-flyspell goto-last-change ruby-tools ruby-refactor visual-regexp-steroids visual-regexp bundler smartparens paredit js2-mode ruby-interpolation column-enforce-mode ruby-electric yard-mode projectile-rails rspec-mode robe use-package relative-line-numbers rbenv neotree magit helm-projectile helm-dash helm-ag flycheck company base16-theme avy-zap ace-window ac-helm))))
+    (modeline-posn json-mode minimal-theme smart-mode-line-powerline-theme powerline js2-refactor go-mode racket-mode multiple-cursors org-trello ac-geiser geiser scheme-complete deft restart-emacs exec-path-from-shell helm-ag-r ag focus markdown-mode markdown-preview-mode ctags helm-flx company-flx flx-isearch flx-ido fold-dwim emacsql-sqlite web-mode markdown-mode+ yaml-mode rainbow-delimiters auto-indent-mode indent-guide helm-flyspell goto-last-change ruby-tools ruby-refactor visual-regexp-steroids visual-regexp bundler smartparens paredit js2-mode ruby-interpolation column-enforce-mode ruby-electric yard-mode projectile-rails rspec-mode robe use-package relative-line-numbers rbenv neotree magit helm-projectile helm-dash helm-ag flycheck company base16-theme avy-zap ace-window ac-helm)))
+ '(size-indication-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#ffffff" :foreground "#373b41" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 120 :foundry "nil" :family "Input Mono Condensed")))))
+ '(default ((t (:inherit nil :stipple nil :background "#ffffff" :foreground "#373b41" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight light :height 110 :width extra-condensed :foundry "nil" :family "Input Mono Narrow"))))
+ '(fringe ((t (:background "gray100"))))
+ '(hl-line ((t (:inherit highlight :background "gray93"))))
+ '(mode-line ((t (:background "#373b41" :foreground "#ddd" :box (:line-width 5 :color "#373B41") :height 1.0))))
+ '(relative-line-numbers ((t (:inherit linum)))))
