b265cac6e8325c7f215f373077fd950e6bbfaeac
Max Murdoch <max@maxmurdo.ch>
2016-02-13 14:14:05 +0000

    Update aesthetic.el

--- .emacs.d/aesthetic.el
+++ .emacs.d/aesthetic.el
@@ -8,27 +8,23 @@
 
 (transient-mark-mode 't)
 (delete-selection-mode t)
+(setq left-margin-width 3)
+(setq right-margin-width 3)
 (setq x-select-enable-clipboard t)
-
-(column-number-mode t)
-(use-package relative-line-numbers
-  :ensure t
-  :init
-  (linum-mode -1)
-  (global-relative-line-numbers-mode))
-
-(set-fringe-mode 0)
+(set-face-attribute 'fringe nil :background "gray100")
 
 (use-package base16-theme
   :ensure t
   :init
   (load-theme 'base16-tomorrow-light t) ; t means it won't confirm when loading the theme
-  )
+  (line-number-mode)
+  (column-number-mode))
 
 (set-face-background 'hl-line "gray93")
-(set-face-background 'linum "gray93")
 (set-face-background 'region "gray80")
+
 (use-package indent-guide
   :ensure t
+  :diminish indent-guide-mode
   :init
   (setq indent-guide-mode t))
