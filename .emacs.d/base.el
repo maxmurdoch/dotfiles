863120f705daf16ad7fe4fb518e48064fddf1752
Max Murdoch <max@maxmurdo.ch>
2016-02-13 14:09:24 +0000

    Remove startup message from emacs

--- .emacs.d/base.el
+++ .emacs.d/base.el
@@ -3,6 +3,8 @@
 (set-default 'fill-column 100)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
+(setq inhibit-startup-message t)
+(fset 'yes-or-no-p 'y-or-n-p) ; use y or n rather than yes or no in interactive dialogs
 
 ;;;; settings
 
@@ -15,9 +17,6 @@
 
 (setq global-auto-complete-mode t)
 (setq custom-file "~/.emacs.d/custom.el") ; save custom variables in a specific file, not .emacs
-
-
-(fset 'yes-or-no-p 'y-or-n-p) ; use y or n rather than yes or no in interactive dialogs
 
 (blink-cursor-mode t)
 (delete-selection-mode t) ; typing when a word is highlighted deletes the word
