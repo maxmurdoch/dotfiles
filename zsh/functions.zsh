a136513fbd528bb5614cc787a1c5fb6008b92a8a
Max Murdoch <max@maxmurdo.ch>
2016-02-13 14:17:31 +0000

    Add vpn function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -23,3 +23,32 @@ function deploy {
 function c {
   cd $(find ~/gc -type d -maxdepth 5 | fzf-tmux -d 15)
 }
+
+vpn () {
+        local vpn="${1:-GoCardless}"
+        local vpn_status=$(scutil --nc status $vpn | head -n1)
+        if [[ "$vpn_status" = "Connected" ]]
+        then
+                echo "Already connected"
+                return
+        fi
+
+        printf "Attempting to connect to VPN.."
+
+        while [[ ! "$vpn_status" = "Connected" ]]
+        do
+                printf "."
+                /usr/bin/env osascript >> /dev/null 2>&1 <<EOF
+tell application "System Events"
+  tell current location of network preferences
+    set VPN to service "$vpn"
+    if exists VPN then connect VPN
+  end tell
+end tell
+EOF
+                sleep 1
+                vpn_status=$(scutil --nc status $vpn | head -n1)
+        done
+        printf "\n"
+        echo "$vpn_status"
+}
