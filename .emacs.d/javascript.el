ffc4bcb413c881a292eaa997d13fcd051672fa35
Max Murdoch <max@maxmurdo.ch>
2016-02-13 14:09:54 +0000

    Update javascript.el
    - Use js2
    - Update indent offset to 2 instead of 4
    - Column enforce 120
    - Rainbow delimiters
    - Indent guide

--- .emacs.d/javascript.el
+++ .emacs.d/javascript.el
@@ -1,7 +1,12 @@
 (use-package js2-mode
   :ensure t
+  :mode ("\\.\\(js\\|jsx\\)\\'" . js2-mode)
   :init
   (setq js-indent-level 2)
+  (setq-default js2-basic-offset 2)
   :config
-  (rainbow-delimiters-mode-enable)
-  (indent-guide-mode))
+  (add-hook 'javascript-mode-hook
+          (lambda ()
+            (column-enforce-n 120)
+            (rainbow-delimiters-mode-enable)
+            (indent-guide-mode))))
