167c59145e788df69ae57a97bd1bf592a42d6ef8
Max Murdoch <max@maxmurdo.ch>
2016-05-29 20:09:29 +0100

    Move keybindings into module files

--- .emacs.d/keybindings.el
+++ .emacs.d/keybindings.el
@@ -1,7 +1,5 @@
-(global-set-key (kbd "C-c w") 'ace-window)
-(global-set-key (kbd "C-;") 'avy-goto-char)
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c a") 'org-agenda)
+;; hippie
 (global-set-key (kbd "M-/") 'hippie-expand)
-;; Allow hash to be entered
+
+;; misc
 (global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
