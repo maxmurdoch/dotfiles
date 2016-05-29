0e066d279607e8680ca23b900696980e79c049cf
Max Murdoch <max@maxmurdo.ch>
2016-05-29 20:09:56 +0100

    Add rainbow delimiters and margins hooks

--- .emacs.d/hooks.el
+++ .emacs.d/hooks.el
@@ -1,1 +1,11 @@
-(add-hook 'before-save-hook (lambda () (delete-trailing-whitespace)))
+(add-hook 'before-save-hook (lambda ()
+                              (whitespace-cleanup)))
+
+;; add 1 column of horizontal margins to every new buffer
+(add-hook 'buffer-list-update-hook
+          (lambda ()
+            (set-window-margins (car (get-buffer-window-list
+                                      (current-buffer) nil t)) 1 1)))
+
+;; use rainbow delimiters in all programming modes
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
