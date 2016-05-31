c8045af4c0e42a9d9aec8500ab1ec1f8336bc00d
Max Murdoch <max@maxmurdo.ch>
2016-05-31 21:39:24 +0100

    Update custom

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -12,6 +12,8 @@
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "b9183de9666c3a16a7ffa7faaa8e9941b8d0ab50f9aaba1ca49f2f3aec7e3be9" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "232f715279fc131ed4facf6a517b84d23dca145fcc0e09c5e0f90eb534e1680f" "7c1e99f9d46c397b3fd08c7fdd44fe47c4778ab69cc22c344f404204eb471baa" "f2503f0a035c2122984e90eb184185769ee665de5864edc19b339856942d2d2d" "7e346cf2cb6a8324930c9f07ce050e9b7dfae5a315cd8ed3af6bcc94343f8402" "a922c743710bb5d7c14995345549141f01211ff5089057dc718a5a33104c3fd1" "3a3917dbcc6571ef3942c2bf4c4240f70b5c4bc0b28192be6d3f9acd83607a24" default)))
  '(electric-pair-mode t)
+ '(emoji-display-mode t)
+ '(global-company-mode t)
  '(global-hl-line-mode t)
  '(line-number-mode t)
  '(mode-line-format
@@ -23,7 +25,7 @@
  '(package-selected-packages
    (quote
     (modeline-posn json-mode minimal-theme smart-mode-line-powerline-theme powerline js2-refactor go-mode racket-mode multiple-cursors org-trello ac-geiser geiser scheme-complete deft restart-emacs exec-path-from-shell helm-ag-r ag focus markdown-mode markdown-preview-mode ctags helm-flx company-flx flx-isearch flx-ido fold-dwim emacsql-sqlite web-mode markdown-mode+ yaml-mode rainbow-delimiters auto-indent-mode indent-guide helm-flyspell goto-last-change ruby-tools ruby-refactor visual-regexp-steroids visual-regexp bundler smartparens paredit js2-mode ruby-interpolation column-enforce-mode ruby-electric yard-mode projectile-rails rspec-mode robe use-package relative-line-numbers rbenv neotree magit helm-projectile helm-dash helm-ag flycheck company base16-theme avy-zap ace-window ac-helm)))
- '(show-paren-mode t)
+ '(show-paren-mode nil)
  '(sml/col-number-format "col %2c")
  '(sml/line-number-format "line %2l")
  '(sml/numbers-separator ", "))
@@ -39,6 +41,10 @@
  '(highlight ((t (:background "#ECF0F1"))))
  '(hl-line ((t (:inherit highlight))))
  '(mode-line ((t (:background "#373b41" :inverse-video nil :box (:line-width 5 :color "#2C3E50") :height 1.0))))
+ '(org-pomodoro-mode-line-break ((t nil)))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "#7F8C8D"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "#BDC3C7"))))
  '(relative-line-numbers ((t (:inherit linum))))
  '(show-paren-match ((t (:background "#ECF0F1" :foreground "#222" :weight bold))))
- '(show-paren-mismatch ((t (:background "#ECF0F1" :foreground "IndianRed2" :weight bold)))))
+ '(show-paren-mismatch ((t (:background "#ECF0F1" :foreground "IndianRed2" :weight bold))))
+ '(sp-show-pair-match-face ((t (:background "#ECF0F1" :foreground "black" :weight bold)))))
